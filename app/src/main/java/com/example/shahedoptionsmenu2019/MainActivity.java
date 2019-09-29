50cdde77df73646db15b7168d506af186f372afe
JamalSamniya <jamal.samniya@gmail.com>
2019-09-29 11:36:44 +0300

    shahed

--- app/src/main/java/com/example/shahedoptionsmenu2019/MainActivity.java
+++ app/src/main/java/com/example/shahedoptionsmenu2019/MainActivity.java
@@ -2,9 +2,13 @@ package com.example.shahedoptionsmenu2019;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -24,4 +28,26 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()==R.id.itmScreem1)
+        {
+            Intent intent=new Intent(this,Screen1.class);
+            startActivity(intent);
+        }
+        if(item.getItemId()==R.id.itmScreen2)
+        {
+            Intent intent=new Intent(this,Screen2.class);
+            startActivity(intent);
+        }
+        if(item.getItemId()==R.id.itmScreem1)
+        {
+            Toast.makeText(this,"ynet",Toast.LENGTH_SHORT).show();
+
+            Intent intent=new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.ynet.co.il"));
+            startActivity(intent);
+        }
+
+       return true;
+    }
 }
